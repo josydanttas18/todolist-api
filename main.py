aeb49892284a5567880a73325036b81a6708931b
Josimeire <josy.danttas18@gmail.com>
2025-10-17 22:07:42 -0300

    Tarefas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Importação das bibliotecas
 from flask import Flask
+from tarefa import buscar_tarefas
 
 #Cria o objeto do flask
 app = Flask(__name__)
@@ -8,6 +9,12 @@ app = Flask(__name__)
 @app.route('/api')
 def imdex():
     return 'Api rodando'
+
+    #Criando a rota que retorna as tarefas
+@app.route('/api/tarefas')
+def get_tarefas():
+    tarefas = buscar_tarefas()
+    return tarefas
 
     #Identifica que é arquivo principal
     #E liga o servidor executando o Flask 😊
